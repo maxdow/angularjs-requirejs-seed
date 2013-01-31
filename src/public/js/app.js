c59c0ef37b6beeda0de8ef8a351a8e7b366e015e
Maxime Warnier <marmax@gmail.com>
2013-01-31 12:04:08 +0100

    Ajout retouage avec templates

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,7 +4,27 @@ define(
 
 	function BaseManager(angular){
 		var initialize = function () {
-			angular.bootstrap(document);
+
+		var app = angular.module("myApp", [], function($routeProvider, $locationProvider) {
+		  $routeProvider.when('/', {
+		    templateUrl: '/templates/Accueil.html',
+		    controller: AccueilCtrl
+		  });
+		  $routeProvider.when('/bases', {
+		    templateUrl: '/templates/Accueil.html',
+		    controller: BasesCtrl
+		  });		  
+		  // When you put /home, it also automatically handles /home/ as well
+
+
+		//  $routeProvider.otherwise( { redirectTo: '/login'} );
+
+		  $locationProvider.html5Mode(true);
+		});	
+
+		angular.bootstrap(document,["myApp"]);
+
+
 		};
 	return {
 		initialize : initialize
