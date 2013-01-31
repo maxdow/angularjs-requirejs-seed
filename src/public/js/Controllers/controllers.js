b204a3caf6ad27e12f65cd6f3e89ea86da9deb87
Maxime Warnier <marmax@gmail.com>
2013-01-31 12:46:51 +0100

    fichiers manquant

---
+++ src/public/js/Controllers/controllers.js
@@ -0,0 +1,17 @@
+/* Controllers */
+
+//http://jacobmumm.com/2012/09/11/single-page-apps-with-node-and-angular/
+
+
+function AccueilCtrl($scope) {
+
+	$scope.test = "test2";
+
+}
+
+
+function BasesCtrl($scope) {
+
+	$scope.test = "test-----";
+
+}
