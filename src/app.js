982fdb806a2f841c20fd9a4eafdd6676850316ea
Maxime Warnier <marmax@gmail.com>
2013-02-22 10:53:23 +0100

    update angular 1.0.5

--- src/app.js
+++ src/app.js
@@ -3,12 +3,9 @@ var express = require('express'),
 
 
 app.configure(function(){
-  /*app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());*/
+
   app.use(express.static(__dirname + '/public'));
-  //app.use(app.router);
+
 });
 
 
